55f06ff9db0285eb532b2d317f06f8e4e1407702
Akon-911 <Mananjerath9582@gmail.com>
2025-07-26 00:03:24 +0530

    Insertion Sort

--- algo/InsertionSort.java
+++ algo/InsertionSort.java
@@ -3,7 +3,16 @@ package algo;
 public class InsertionSort {
     public static int[] sort(int[] arr) {
 
-        
+        for(int i = 1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i-1;
+
+            while (j<=0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
 
         return arr;
 
